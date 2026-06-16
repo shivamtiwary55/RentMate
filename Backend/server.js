f5bb0b6c84ebd9e5286c712650983bbb9c6ac92f
shivamtiwary55 <shivamtiwary.aiml.aec@gmail.com>
2026-06-16 15:31:01 +0530

    Added server.js

--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,8 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import { connectDB } from './config/db.js';
 import authRoutes from './routes/auth.routes.js';
+import listingRoutes from './routes/listing.routes.js';
+import quizRoutes from './routes/quiz.routes.js';
 
 dotenv.config();
 
@@ -14,8 +16,9 @@ app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
-// Routes
 app.use('/api/auth', authRoutes);
+app.use('/api/listings', listingRoutes);
+app.use('/api/quiz', quizRoutes);
 
 app.listen(PORT, () => {
   connectDB();
